2c09876c07f6d53ad4ca52fa2dba811d513c1385
Piotr <piotr.mamenas@gmail.com>
2018-07-01 16:29:35 +0200

    routing and routing components. Shared module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+
+import { routing } from './app.routing';
+
+import { SharedModule } from './shared/shared.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    SharedModule,
+    routing,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,20 @@
+// Angular Libraries
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  declarations: [
+  ],
+  exports: [
+  ]
+})
+
+export class SharedModule {
+}
