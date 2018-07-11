af8dc9006c52f2293ebe6d4584d5df91e36797f0
Piotr <piotr.mamenas@gmail.com>
2018-07-11 22:50:53 +0200

    initial logic for guess committing, adding dummy data. styling.

---
+++ src/app/dummy-data/translation-data.ts
@@ -0,0 +1,9 @@
+import { TranslationRow } from './../interfaces/translations-row';
+
+export const currentTranslationRow : TranslationRow[] =
+[
+  { id: 1, english: 'hello', deutsch: 'hallo', francais: 'salut', espanol: 'hola', italiano: '', polski: ''},
+  { id: 2, english: 'output', deutsch: 'aufgabe', francais: 'tache', espanol: 'mancha', italiano: '', polski: ''},
+  { id: 3, english: 'in front of', deutsch: 'vor', francais: 'avant', espanol: 'delante de', italiano: '', polski: ''},
+  { id: 4, english: 'swear', deutsch: 'schworen', francais: 'preter serment', espanol: 'prestar juramento', italiano: '', polski: ''}
+]
--- src/app/trainer/components/guess-input/guess-input.component.ts
+++ src/app/trainer/components/guess-input/guess-input.component.ts
@@ -1,23 +1,47 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
 
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+
+import { currentTranslationRow } from './../../../dummy-data/translation-data';
 
 @Component({
   selector: 'trainer-guess-input',
   templateUrl: './guess-input.component.html',
   styleUrls: ['./guess-input.component.scss']
 })
-export class GuessInputComponent {
-  isCommitted : boolean = false;
+export class GuessInputComponent implements OnInit {
+  isCommitted : boolean;
+  isCorrect : boolean;
+  currentItem : number;
+
   guessedWord : string = 'Hola';
   actualTranslation : string = 'Hello';
-  
+
   guessForm = new FormGroup({
     guessedPhrase: new FormControl('', Validators.required)
   });
 
+  ngOnInit(){
+    this.isCommitted = false;
+    this.isCorrect = false;
+    this.currentItem = 0;
+  }
+
   commitGuess() {
-    console.log(this.guessForm.value);
+    if(this.isCommitted){
+
+    }
+    if (this.guessForm.valid){
+      this.isCommitted = true;
+      if(this.guessForm.value.guessedPhrase == this.actualTranslation){
+        this.isCorrect = true;
+      } else {
+        this.isCorrect = false;
+      }
+
+    }
   }
 
   overrideGuess() {
